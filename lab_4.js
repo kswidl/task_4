317fbdad4713e384a1683b2a441a3ebf0f85fa13
kswidl <kitabakim@gmail.com>
2026-04-05 20:17:25 +0300

    add search by priority

--- lab_4.js
+++ lab_4.js
@@ -23,4 +23,50 @@ class BiDirectionalPriorityQueue {
   size() {
     return this.items.length;
   }
+
+  findIndex(type) {
+    if (this.items.length === 0) {
+        return -1
+    }
+
+    let targetIndex = 0
+
+    for (let i = 1; i < this.items.length; i++) {
+        const current = this.items[i]
+        const target = this.items[targetIndex]
+
+        switch (type) {
+            case "highest":
+                if (
+                    current.priority > target.priority ||
+                    (current.priority === target.priority && current.order < target.order)
+                ) {
+                    targetIndex = i
+                }
+                break
+            case "lowest":
+                if(
+                    current.priority < target.priority ||
+                    (current.priority === target.priority && current.order < target.order)
+                ) {
+                    targetIndex = i
+                }
+                break
+            case "oldest":
+                if (current.order < target.order) {
+                    targetIndex = i
+                }
+                break
+            case "newest":
+                if (current.order > target.order) {
+                    targetIndex = i
+                }
+                break
+
+            default:
+                throw new Error("Invalid operation type. Use: highest, lowest, oldest, newest")
+        }
+    }
+    return targetIndex
+  }
 }
